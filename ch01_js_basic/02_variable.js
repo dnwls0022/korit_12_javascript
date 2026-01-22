9deca8afff057e82e7299b71d7f8c0c56a95d56c
vistaviezq <vistaviezq@naver.com>
2026-01-22 17:46:43 +0900

    feat:20260122 js variable / operator/ types

---
+++ ch01_js_basic/02_variable.js
@@ -0,0 +1,16 @@
+var x =5;
+var y =6;
+var z =x+y;
+console.log(z);
+
+var x = 7;
+z = x+y;
+console.log(z);
+
+console.log(a); //오류가안된다
+var a = '안녕하세요'
+console.log(a);
+
+/*
+10번라인에서 선언되지도않은 a란 변수를 console에 찍을 것을 요구했음에도 오류로그가 뜨지않고 a변수에 저장된 값이 undefined라고 이야기한다.
+*/
---
+++ ch01_js_basic/03_letveriable.js
@@ -0,0 +1,11 @@
+let x =5;
+let y =6;
+let z =x+y;
+
+// let x = 7; //이제 오류가 난다.
+
+x= 7;
+const a =1;
+//a=2; //오류발생
+console.log(a);
+
